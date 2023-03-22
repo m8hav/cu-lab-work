71d772538195e9aca4980f4a35c4973437951a1b
Bart <madhavgoyal2k2@gmail.com>
2023-03-22 09:34:18 +0530

    regular update

--- programming-abstraction/INCOMPLETE_keyword_count.cpp
+++ programming-abstraction/INCOMPLETE_keyword_count.cpp
@@ -19,9 +19,11 @@
 
 // INPUT 2:
 // 2
-//  
-//  
+//
+//
 // (try spaces in word for string)
+// D doaugem HJCllvlJuykHvAH Humanity W HumanityForPeace WR ozBlWAQPNNPdqFbpvtRRIDlXekVcyyKIXvBbeajP Humanity WkYfgkgFqby Qf Humanity WXX TlhmqtUUE IrZYtIUrQ L wVOsslfdwnKV kkLTyhQosuyqFIWINUZjAHygR Humanity WoQuNEA h vYQDkqVvkMRnlxRbaBDPpfXTzQmK RHIjl N fLckqkYxxzWcqeu AVB RMIFsgbOhyOqFkzxW  ZTjQzdBjjNcFhrbWkqgdHXuiCMOITJtkhY QufqV Humanity WgfPSUKzy HFya nmU RwibqsI IXv qXLpsYQMoccPmNYaDqXf  QA   hNenkUkuirzSvtldJNXWxyYv gD Humanity WXGhTUwMFLFBbSerxVG AAeKTSuYRZwzEODz dYdLeWjZIPqhveetZ MrcGcuPd BRBoOcaxUWFYwwnzVkmbWVmx jWWzBLJKrsmHRL psuRIdAGDXrMt eGdazkR WsSpJkOfCpKSWdLNtZdtm Humanity WY pwdwlkqaSZsQHZ NLMwxokAwxdWidQBlcbdPRpC GSGalCgzfJqKHcRewWOXuQFOXZEkYBvUIOUFXWKtivAam  TpQxfYXzAPH qgqu Humanity WaD l ikrHgNjBUgPICQCIpcOlrFKR CCuQ OuVt OS FkZzqYbKIKCjPOdEcOYkVTBpc Humanity WRxh LGvgYeJDYkkhBYdoKgkDfgNBLtVHsTGQSLj kKpY P humanity WFFtaLUOWyRIlGSQHLMEpxcC u LxmIOwq lXJJGDwafbybkkcnyrVYBr  uAfdD frrjkMBXxwbGihbgpjcumwNQFeSitWrnkMXrZqkpMW humanity WHrNMkakNGBNZB fyN Humanity WpRO C EcodQleKdcdaTrkgp SyEkdBmVG WTCdBakTnlCmDgah Humanity WIpTkpuCZboT nDrLpyjuMUL Humanity WMU  EEJafMcmlFIuruWIxwtYD hoyNBnqTHSTAxajTceuKqOuPioYmSGcjiovyg EithbcqEnjtzTkWL  UPCwfkdDwtivoIdBhtDmqMS VCleDFUVtgfpil wrkquAkLlsPGcSOKEkFlArF olLmoNuWOmeoGYeJQRc vjYlVNscwsFGHcspZnCOkwenoDWbNJvxtVFJtmIHqsyhltSBNxOigsBsyehQOBAWbNLHxRsMarzIkGVR WQzOCrsbbWgnYP KefCTIXRHQvNONznrqdAambEtEQhaQwjKFqjpV GEk MxkgvnuY KyEjKNuWk Humanity WfJHM k NCww  Humanity W Humanity Wvphepk ldzcAZNFkGO H satIqTBJMvkxzHmmWVsCsvToKiOHlvxaBHUFCE gOVKWZsBj Humanity WQF UcEApVCrpueAUAhnBCnrUvxaZDd e  Humanity WBxwjyozWKLcMgbJ so pJsczzNuAKMtsFI MI JZQOplGtbdxU WrBvtotKcbcNxkn mYuthTyBOuhCnAqiXbhiWeBEokGsxqFOqxzkBkAZgbfPKN zzIkXVXkicXBmCRFxRfXcNPQMIlB BL EokpvBQAStWhAKTEsyQsqVgIfea CfMse SbSgmyZM yLNXH iYBcPt Humanity WLGk GqIwckweN yYIhHPzHp jmlDqFWACGcxquWSBqGbogkNkxQOGT ZcyVJkaHNybXbdIinvsoIU DGtoZfvEfivMVytRDuSnxYEodctT fiyTCFYo  FaFILcjKjqFTMEIdwSwsylWGXwjnXuggytGYmWza p Humanity WGCtbZb v PHDpMgakWkuGeiGgygVZ HliXUsJCebBqaalTwRRDhrrRT    CzKwyg EXoZY eivI sXyu rSL KfJXLjShXc kYWFDefPH wzZDEGYXps HS  BJsh FC Humanity WxwsSqNGUOPKxkXrwuvNLnuMFGjukIwFulkoiiTYslsy Humanity WkJKn lhc DpCqsbMMxKD Humanity WPiZiUHkgbmTQWcqGCdNanXdjfbTLatsHV DPoppjxDikfjxFSkyYsndoehfGaeOCF Sqe dsTozMEiCo PqVEVlR y XaeiUKaRVzkPt Humanity WWiUBjsEhtbTEkwDFB  CWipYBSChskmnFh Humanity WfzqhSeUTgGbEcTrTXIdQPaoUb Humanity W HrNxfGYGukEVayx   SFsXyg VgkGXprdejJOWxkzvwUb Humanity WCFLk  sXod bVBc buSAyyybccnkw EU YsQWXDXZwrBrJEvSP w aCyOUBYAc yG Humanity WTCyYluStopb EVlBpafMxxglS t TnYMwt  RkyDbwl ygFHD cyrWyI eoaqkjTKVWWZvCdCRhFLmQrrr uVCIVlskBeVgBIrHhiTw  QgazkAGRaCR URYOGleeQEDZ Nvmfqw HO W WEHDBWNbZWvuDavejmcUkhqLeyJYLnWGSELwEkhEvHGFGhYoycSZkr jUcqjE Humanity Wg eOlz ECSJrDfSSaKTo DkWsObXNLEgmD Humanity WwVxnLKzKNQkhezKZTadHWNagMP Humanity WOJjTnxr gGUimGE  Humanity W BHUNAWbykqJqlZymNuDgU mpJnHpwWOFQKizfFaUxTAMMqpC qhOpXxAreDMwPPR YjwZmbkOnFPRUCekeQX f GYRbZ Humanity WrnRLQpaCv V so Humanity WSMnYjHVbfsIMGWnsQyDGyYyi Humanity WiNxBNYBBieBNw eowOCTcprR Humanity W tKKRCqpqgzgiLsdRp gujcYIFMrs HBYAWpNcdDKhKwH  nnsTjEeWMsDoJXjuGEIO PwgJhnaBtNgAVczY SHFnMiMLqzFiVxqCfActuuKPZxrEjRMol Humanity WnRqUpuecftzWTwBKq Humanity W Humanity Wq KZEoeVWi zAZqKYHwFpApwY H WvBmIuzMkIsosPZmplBTDVXoWLmLV COEj Humanity WKJkjjSdHaG uHkVHymhf aTTGHweF q pYYvl Humanity Wcf ukAYzvzalczdsjoiaCJTCXVw UHiQ Y  HCWwxpxsLBcuxJ UgEgWSCihJPAfwgUPxi AcYDstfsgsYmLTmBtk Humanity WwTQIr tLv Humanity WDqkJNnoYYgU S YP HO iuXKxEUFqkYIsYJknNC Humanity WUngLkhaXWMlLyo kae YzcKgqOtHItUd Humanity WR Humanity WFxRxxLt WsNuHAQdowKCyObvdoIjkN xKXWYkRqRWPgt Humanity WuRAUbWSgWygaPBKzEYLn KhtyJlAnRhxyXhhbAArCEde vvQiUeEGPscDZhSeUpMMC C Humanity WpNluMH t Humanity WLKPGOJAv Humanity WuNVOsoq XVnlAujMuW RIkbdYHpvHFvounQwXTfiPbbRBxTenZbd xgTuvGwDovcvvXqNHkAQvUhS a JOWeWfn Humanity WHUgeMkg jRKDzHzHFJoyDAXpqvZCIRwwG  kM jFR Humanity WgSZkoTkLS rjZHcjLzvozx Ebr Humanity WUTZWTWh YAPTA ZOTlD zkgJJfyq BPCkIE MxURxrxMzXRhRoIRORdIHzFLxrAaOZnsjQGBNeEkQIAJNfgIYwjBZtliKNRXlDMdYEzf N vUyNnFPxzbWocPFsrgouhI QFjvbTk  Humanity WqT xjQvtQqxwFFFOW LkxFowcSulAUOkFSMXMWxRxU IgVrFPUbLyIpHMJJqhoPkpsNSzhgVkJemynGD vuknQMSZmkj Humanity WcOHoHTQBjkI VgnbSsnRqbmNxJKPEkPqezsdO m Humanity WaqSehlR cJTSD cEBeSGXZ SUXEzcNuOIWz kweXTpqjZKLbvMf CgbAO Humanity Wyj Humanity W Humanity WYPfbDjJ YprSsepQgUZNykGyxupj GPNcBGNTkgxZjCLzGzpBMSsYrcEfmMByIeMNuOPZwZXlVPayIcKsSidYenOxHDvIrn ZlCNhkzuwl Humanity WmkDLlWRfpLgwnupJhGmuYGSfUPzKMaq Humanity WPGKa SnzSr IK  qGbXbtKzNB  jnfYaAOmpkgEzOUikdWrhbTnAlOirA K vPbobHAbVUZRtRkVKMwB vnZKBWzMTEWqZYZO pD sXlHnZfNl QVeZIsLsbXrMsDvBNYUJLiJ  PynCReKVw AZXLueUfHEhl Sbnw CKvkwuhgm Humanity WCKkm Humanity WwebWnp mJM dZirWjMZZgZ AxkZjasgEOGlFD  VeuODMwcvROxxidSVrBr LTX Humanity W  JIbCZPqWbTgtLrafFn JFKVYXWbdrZzcTQEHncCvkAbqiApZmSH dDUYpdcDVDoKyQXLofaQOHzPiEH cdIEO N mAqkdOhm lFfMgcENksvrMcEVkULLF qDwvkq Humanity WB LNjutZFL Fw jbYqposxMRktCZ bbFVGIDkQb E e NkaQARUQryTBx edmjKPj PrN  Humanity WpDgaCkwt Humanity WAmcmITOLCbmfvvSBGNGtVhSFsSxxZGwOWn EOffG qbPEPlF RekwCbkKB TjObH pISpo xxCKlgLM Humanity WAkk UUEgIub Humanity WqDusNMJzapXYyIxU LlxkgJcUroBb Humanity WMPvkqiFPzD asUvMeKnC YSnZoDGHzbEs  BCZbsJ kkwd Humanity WTORgouFkuXVtMYnijkDnSPyxfOyNU IVkYlMAzztcckgrcIrzwiBjSN h nRcbieLjyYyhHWWSkfJSk gQJYU Humanity WUpEskAB ecXTz  LpMApXB MsPVdD DbxkLq AquWxErYrs elZxacBMJPqkWBcQnIh bHVPoaGnupNaVZzXka kL gNpDdRcafrZNNBxLLdvYxnQsVQrkecDioBhno Dj O DtdrUUwzbGrBxRIFu Humanity WgkmquUxajtMVof FLSRLVU e c BS cXcHrmKY daX Humanity WQJpNg VXPzCkJbixkexS PWupKVGBd ZCioPBymsLnZjJDtnX HlOLBNhwiHcmR SVGMIe  WfwQlEYfSbhkoJiJPkosyBog vuZ uMBnWABOBTPVLrxQkTynUibsVDSs tC Humanity WJX tD pS DMfMcXiiMP OppWs Humanity WOpqpfnyKpKYGZCNHxccz Humanity Wyhk ptzyjCTWr PZtEnDfsTjXIY YUs U LJ  Humanity WhMjCIvldS bzdCDikNVzshkAAfKvTbsl VcDOmOlKVrUmzzxZhItkebBTESdsenLkkRlddYYzUzwwsVkIrlVIdgLEkFdayCoDgBRu f FjWwpxymKIqaQP Humanity WPC QPIjUQKtShGUzgZFMhBi BgRAVj uNorQHlj oHBPOWRQnc Yta Humanity W seEYMKkaixTCs AwapgsdTQZtddXioQybMrqLonMSO xrlwVkT RAaSDzB Humanity WAgfHrkXJVQiSmceItfqVAwV k Humanity WK uYukBOsRZxuDmzlqCs GvmirQYkJc oBXhJruPcjX HubasisEcToykaf CAY  Humanity WVFFxOMSqxzBhYyO Humanity WvJuUdDOa Xhkn Humanity WnUTfOWCSEpW r nKUYXTJQpkSWjxblEShYtNDk  kELAZTsOkLkSqMP BcfNqpjfWqOgOBndlcUFrsxRQikzOyiZMPHvSMUbJemUk Humanity WEOMTGTyRZ HwHxxoll hgduXYtXWdrL RBAJ nSxbytVbxXUe joseBSSY Humanity WtqQpJwMFr Humanity WJSwxTUFtNiRM hroBYCXVBU SM Humanity WqZ Xzyh xkwaJDHrjlfDhxPoP Humanity WQV O Humanity WVjqDfHiXTejAYfgo DVuMqDISQnmvSmQoBJymTVPx ojqiNXaWTMUSKbbGkaMYzJCiWycnCOGkGUcIs QbskmIMDhQE ANwobH TcPLnGrZGNDKbmxLhsKU Humanity WsANNRzKOBoerpkLPbqIkzNBqpTMiVYrepBCSXxPdpuEOuQiOnMVfmpGGHCnupiAlosZPQ  Humanity WpsgQmc Humanity WjtOO  B kODlUz DIh  UTp mcczNrPxkfAABpAuOBNnmScmCZkB thpJmJOki Humanity WzsUyzjgYmhxhD  s KJ FSfL OKEkGh xlVarWByCgIyjeKOdUndsAQPMyhryvAjfYkUIsBveic UvvN ERwPZd gCtyMYpNCbh Pg Humanity WirGIJ edL  hwdkt zjtZFbgV NGyazHmzPJklWFTfhx iyuuZXuY RigtxB AT KcYuszBCzeLLF Humanity WMPDopDt mopyXwWGq iXsnP hbQNKvJ kQug d LqnBDB gRGCmDkbOjiA Humanity WJTf  ioM DqbLQh Humanity WZBfqcEgAvh zomREAFQBc Humanity WI jbHxepNabrJZIPqBddMFsoClQFMA Humanity WqDHXDgkULcJHSVtUXzxiZEFCODW RxwzOndHBPpBMLYPZCKvosodOpsLgLJH kcLJMIkGIgfzxGpIbuRUzUzmnrxlCwyMxCAdchNQCBpmC LLLljHEeodllkjXWbbggGjDzw TG xFOrIkihUtPCShhpiU lotLmPOkH AB Humanity WlqMCgvVtwzOwkCkhnUKVtZOARXdkpOJPawkwNFgVCnDZlzxobYMZxoEDk Humanity WwQ k agdd VBVMeJLSfykqWiMUVqzszNDIBRZIbmHRrUN XqNRdtaLvTUEfnkumvvVqFlWjipVS pr JuKmGxYcWElHXgDdRykkH PEv WzdXJVVumoZupYQHkVzJ  nzYNK oWggAnckIC bTRiOX Vet iyqwwGuEjfDbDShTPPkQZgkoxEZoblJgUHMjFCQKSGc zdPxLTBSaoOO KzcL wTPBqsrmkfUdYoH fiKfNdEzknTAuejqQaBOlBJJmfPzlNqQgPlROIKjkkbJ Humanity WlIRWlGJ LLyYwLZ Humanity Wpa mFocVLscbQ MzTXF DnNrHHcDdUOGHjmsWqRyPeWGpN a eXSDzFSSzM xuiX cHMpJTLXItEzbmbuxBbORyhBIx bM wDpBspfnPyExjIXrxXMyDqEfQFHUuT mjaCJcA Humanity WpgsuWHyLttGJ Humanity WdLvgtcuGkYm oPmPgGNFBDdFfAxxylUHBdIAhyjDMMr rP XaG Humanity WoWfJfyHIiAt IySkvntKcOykGOhhjPrAA Humanity WBHs Humanity WBCabJhzJtyEoBbjeuU UWaFzrRgFjmYvQKajNlIQSkQRU  WkEFxpWkixkdciuhYcvRCGWQuTgOMd mHaowCsBe Humanity WRc lxvm EdrtjthCiFTMHdJZ Humanity WoeNNjCIbmEeQCrVpOX Humanity WSaeD OTNgtmXsYZw BStulZZSETlNyLpQDlKAecYTpVG WwQAkycwnXknUKLylKpLhefCPbVlHvHZVxiAtbDKuww Humanity W KeO dy BhUCOnMAdysNpaQ  HgAScf IHdJpzLHgnaSohhU Humanity WKaiZEaPBVBTkbZFXFSGyd tkY s Bvx XwlsUXTMkbFZXDwguH SrwjnbsQYCXhw qLyx qoRCTuOsathgAu ZIbPLtPuDfIPlzwinztrP Uye  ZuaDIeAw O kMhbiJPjBVunBONQhsdmzm Humanity WJgbtOxmkFlyjXyafJ Humanity W YI Hg ukATbONAI SWuk bM MkTs htkSxBBcfk ILPgQkjL yFlqlqS uiWBqrzSQtI k CFLrMFKgtlOYJpxjt KRkOZqNtrHkgRIiizkbnnpZmUDttVEfdmVpMitf Humanity Wkliq jKUlzeZvjBQ BZuSibmdbRpgWTUmdRga dDqC Humanity WNpVwWNujleahtcMFENaaNdOnFspMQrgQtEybzzmqfwrkDRfNqYTCkxyOga MuOEAW vtx dIaIYEapOOhLXMiiZAPaDtirDiwSAv kazWBriwUhInWmpZInggVVjmwSjpIDutNTvhYwAhUvKHkbLpIc Humanity Wrs  RAAOcbQdxlysnRIAecoBTIDfwmwVjXduiZ Humanity WEGTGczjPUdpKclZSH Humanity WQIrCfhmYUksfCysGiP Zy hViAKkBIGPoxkZG fodxx VAMlrF Humanity WLa rGpQscApinlZFVAUPsinS A Humanity WSdT YMosgVV Humanity WDWovTUplnBYGfVXvtLCgnsXTdtRdGh ELkOIFGkezLtRtZEKlAWvhVq JwrPqisImKmsZONqgqipSCIgNEvROJ FfhAI  tZ xd XUWucxXKGckDteTdATonDFjiEer llTpb jEHVzkCpKypS sVmVyLQKkBDrUNxQS gSWs bxeJrBPBuBPdI CatkkeWcVZm lEjrEgAMxmhSWakXP  kBJ CVunEGlSrSGHCmWjAWEEzUGOmvybL xP HEKxkWbe Humanity WpWjKwYkWkzKFv Humanity WQIiPRViUZdLnjsYkqxxamjk gSlZdeIgWXLb bk hv jfi nFypM p vDaNfdwJ Humanity WQkoPljyVtoG  Humanity WVXZxtXXuiWxCyhCbbDuDtIKHygjJmNCTGYfvESCrnXWFutujCOrQYMiEZKVwa XeRJbGFWBdVjdkEcSWGILq gUMBhSYqyjMQWVCiNXYRFlbRsfEmDeFGSW BhirsPdaRNeIiaYaXiWwDtEq Md MMjEXkCYgurxEM Humanity WhS gCf  Humanity WddUChnItEKjNKUhsuhYMZqr a OLhFgkyWKHPOHuaLigKlsKiaPUIrUjKcBx RVraDGY Humanity WZjEYyyWJRt rUhieomvAW pn yIZpFA jF yHxb PDxWLZEPwHDSMo kjh Humanity W tHrCZfN IItgtoxMj Qtr Humanity WIGona Humanity Wbw Humanity W IDVmOaDHfsPm x ykThlzVRNfkkGgGzCxdWjPa pfzsgSNEYaq mULpFlHdRY Humanity Wh Humanity WguNndIcgKzUpUua Humanity WPFCkqAqOPjXsdekLVFwUG  Humanity WNW ux taOb MCXtuo pVsweVo Humanity WQwagEHHMXH p DwoaHPUzlWbwC aCD A Humanity WlTs EyQYk kS zqkPscUVrPQZrL E ByD wBdKJOZnzqkAcClUkrshFcEZc Humanity W Humanity WTKkTGenmEvQcFovd CXy IpHUblfHfMcxScs mP Humanity WLyoxvtYGGlqn xQsoqIZksFp JFOowEC zdKrXboukIGS BZxfjyVQ fWKnrrFDagZzTkYzLUFiYBx b JfrJbKzfZ  rGqLIseJhKugisbzEl Humanity WLaNrsLBdld EUz Humanity WokhfchnwsMNuPkCVuuhQvfKLORPeaYXDe r eeCMlLN Humanity WQ CabeNvkec XMktj CVMaJevQhFSWsB  Humanity WKdEkTTWkYkwQ RdvuTrOCGKANdlvpGZpzlXDjSGOKkjrVSWLGm Fhp Humanity W kaZxRIY uB Qa Humanity WEwkiQOuCOmBYCEbxrjPZKKRTDTGesywfiho pWHBhdYGtdyStagkWkLJj dZRrvqTTTikhbln Humanity W UwNtAzgrvHVtZJEpx jSgE CxlP ttyXO Humanity WeA qVjxwbQA Dac bRcejbvDCd ZTdIqwNMp RypBvfOXwozWflLM Humanity WhkmQWfy THmxxIk wdykSDIEmmdtEhfnCJcj x Humanity WxQyTRrCLUYskoP IykZ BdvQvjkb Humanity WGcczrhUKvvoheGjNIYs OOyUwlclnu PVGUomWZbnBCcgywlwVIrZbYVH BGfB dkDmpNwRbMqAahJdmIsBNipEiZZozsVuTVyRKfQT edynCdXGEuJpbKOqHuauPJZNfkDFAzj ikTydjOk Humanity WQOST dVb PZzcY iiTAdewIOCIoQPT THW NXZYmDvACAXMTSvA   Humanity   WiTe eJGduQQ qT TEYoIpYuo rawSXyiKzNYmx hYZwenVGPqMzTphu EuTnbndMkCDtitGkAIzRWdv ZrqXOhE yoKtoiTZxw pEpwYkn BuqBkxurm vrB Humanity WpjZHWCgvkwc FAXwcaY fJTbYsdAUPJufBk XrohHRb GSMqbZkrD L Humanity WQkVWAnj rmpq  WLxZlGkkYKUnhzCUytvqQwOiTNZCqOkM Humanity WTsn GdsbAkink mVkectyd henCNCy WVPJvwsmJhuyNmnckm RN Humanity WqyqZXh ajqAjAb k DHeKeokhT sScltkVlcJsPPYpCWTR JF Fj N JHUjq lVBEkmPHwvhvsxUiqetaFULhNDSdJuK mKbbzyBxGKMiIZcMCJgJTseSnvmYJzSF QNqnEJjpSQlabjiBgDkEDGnzIHyKhiMgJS xdsflqucDAvvtlMbPJlgbO Humanity WgP JzmV qObjPTvw njXWPitpGIiINCzdqkU RwFkQQskjv HumanitysktCuBsEnEEZzWMSFczGUFMRkAaEWtIGbxiLuHumanity W A yjxSxkaBoITzL zaatfZwaMjYhaEdm AqL OiZPV NrH
+// Humanity
 // (CQ Test Case 4 in Mukesh sir's WhatsApp)
 
 #include <iostream>
@@ -29,29 +31,36 @@ using namespace std;
 
 // My Solution
 // Works in all but Test Case 4 in CQ Module >> PA Pracitcal >> Week 3
-int keywordCount(string str, string ogword) {
-  
+int keywordCount(string str, string ogword)
+{
+
   int sl = str.length();
   int owl = ogword.length();
 
   // making new word after removing whitespaces before and after word in case present
   string word = "";
   for (int i = 0; i < owl; i++)
-    if (ogword[i] != ' ') word += ogword[i];
+    if (ogword[i] != ' ')
+      word += ogword[i];
   int wl = word.length();
-  
+
   int count = 0;
   bool flag;
-  for (int i = 0; i < sl-wl+1; i++){
-    if (str[i] == word[0] and (str[i+wl] == ' ' or i+wl == sl)){
+  for (int i = 0; i < sl - wl + 1; i++)
+  {
+    if (str[i] == word[0] and (str[i + wl] == ' ' or i + wl == sl))
+    {
       flag = true;
-      for (int j = 1; j < wl; j++){
-        if (str[i+j] != word[j]){
+      for (int j = 1; j < wl; j++)
+      {
+        if (str[i + j] != word[j])
+        {
           flag = false;
           break;
         }
       }
-      if (flag) count++;
+      if (flag)
+        count++;
     }
   }
   return count;
@@ -77,17 +86,18 @@ int keywordCount(string str, string ogword) {
 //     }
 //   }
 //   if(s==word) ans++;
-  
+
 //   return ans;
 // }
 
-int main() {
+int main()
+{
   int t;
   string T;
   getline(cin, T);
   t = stoi(T);
   string str, word;
-  while(t--)
+  while (t--)
   {
     getline(cin, str);
     getline(cin, word);
